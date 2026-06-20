7a7b990e6dec066d312bf8fe0aeeaedb24039c3b
polanskaavaleria3-alt <polanskaavaleria3@gmail.com>
2026-06-20 22:04:22 +0300

    Update solution.c

--- problem0-mmap-find-substrings/solution.c
+++ problem0-mmap-find-substrings/solution.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,21 +7,37 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 
-// Программе передаются два аргумента: имя файла и строка для поиска.
-// Необходимо найти все вхождения строки в текстовом файле,
-// используя отображение на память с помощью системного вызова mmap.
-// На стандартный поток вывода вывести список всех позиций (с нуля),
-// где встречается искомая строка, по одной на строку.
+int main(int argc, char **argv) {
+    if (argc != 3) return 1;
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <filename> <search_string>\n", argv[0]);
-        return 1;
+    int fd = open(argv[1], O_RDONLY);
+    if (fd < 0) { perror("open"); return 1; }
+
+    struct stat st;
+    if (fstat(fd, &st) < 0) { perror("fstat"); close(fd); return 1; }
+
+    size_t filesize = (size_t)st.st_size;
+    size_t needle_len = strlen(argv[2]);
+
+    if (filesize == 0 || needle_len == 0 || needle_len > filesize) {
+        close(fd);
+        return 0;
     }
 
-    // TODO: откройте файл, получите его размер через fstat,
-    //       отобразите в память через mmap,
-    //       найдите все вхождения argv[2] и выведите их позиции
+    char *data = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (data == MAP_FAILED) { perror("mmap"); close(fd); return 1; }
 
+    char *p = data;
+    size_t remaining = filesize;
+    while (remaining >= needle_len) {
+        char *found = memmem(p, remaining, argv[2], needle_len);
+        if (!found) break;
+        printf("%ld\n", (long)(found - data));
+        p = found + 1;                      
+        remaining = filesize - (size_t)(p - data);
+    }
+
+    munmap(data, filesize);
+    close(fd);
     return 0;
 }
