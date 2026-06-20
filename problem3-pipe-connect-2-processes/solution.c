5b3c4da6a00244520e4a4197c342af860929e3c7
polanskaavaleria3-alt <polanskaavaleria3@gmail.com>
2026-06-20 22:14:01 +0300

    Update solution.c

--- problem3-pipe-connect-2-processes/solution.c
+++ problem3-pipe-connect-2-processes/solution.c
@@ -3,22 +3,38 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-// Программе передаётся два аргумента: CMD1 и CMD2.
-// Необходимо запустить два процесса, выполняющих эти команды,
-// и перенаправить стандартный поток вывода CMD1 на стандартный поток ввода CMD2.
-// Эквивалентно: CMD1 | CMD2
-// Родительский процесс должен завершаться самым последним.
+int main(int argc, char **argv) {
+    if (argc != 3) return 1;
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <CMD1> <CMD2>\n", argv[0]);
-        return 1;
+    int pipefd[2];
+    if (pipe(pipefd) < 0) { perror("pipe"); return 1; }
+
+    pid_t p1 = fork();
+    if (p1 < 0) { perror("fork"); return 1; }
+    if (p1 == 0) {                         
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[0]);
+        close(pipefd[1]);
+        execlp("sh", "sh", "-c", argv[1], (char *)NULL);
+        perror("execlp cmd1");
+        _exit(1);
     }
 
-    // TODO: создайте канал (pipe),
-    //       запустите CMD1 (argv[1]) так, чтобы его stdout → write-конец канала,
-    //       запустите CMD2 (argv[2]) так, чтобы его stdin  ← read-конец канала,
-    //       дождитесь завершения обоих дочерних процессов.
+    pid_t p2 = fork();
+    if (p2 < 0) { perror("fork"); return 1; }
+    if (p2 == 0) {                         
+        dup2(pipefd[0], STDIN_FILENO);
+        close(pipefd[0]);
+        close(pipefd[1]);
+        execlp("sh", "sh", "-c", argv[2], (char *)NULL);
+        perror("execlp cmd2");
+        _exit(1);
+    }
 
+
+    close(pipefd[0]);
+    close(pipefd[1]);
+    waitpid(p1, NULL, 0);
+    waitpid(p2, NULL, 0);
     return 0;
 }
